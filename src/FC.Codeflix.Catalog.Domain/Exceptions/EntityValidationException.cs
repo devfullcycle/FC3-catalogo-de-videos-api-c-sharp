572c13be66e3421e6850b3430c45998c98b742cc
Igor Gomes <igorgomes96@hotmail.com>
2023-08-15 16:28:46 -0300

    1.02 - Domain Validation

---
+++ src/FC.Codeflix.Catalog.Domain/Exceptions/EntityValidationException.cs
@@ -0,0 +1,9 @@
+﻿namespace FC.Codeflix.Catalog.Domain.Exceptions;
+public class EntityValidationException
+    : Exception
+{
+    public EntityValidationException(string? message)
+        : base(message)
+    {
+    }
+}
---
+++ src/FC.Codeflix.Catalog.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,20 @@
+﻿using FC.Codeflix.Catalog.Domain.Exceptions;
+
+namespace FC.Codeflix.Catalog.Domain.Validation;
+public class DomainValidation
+{
+    public static void NotNull(object? target, string fieldName)
+    {
+        if (target is null)
+            throw new EntityValidationException(
+                $"{fieldName} should not be null");
+    }
+
+    public static void NotNullOrEmpty(string? target, string fieldName)
+    {
+        if (String.IsNullOrWhiteSpace(target))
+            throw new EntityValidationException(
+                $"{fieldName} should not be empty or null");
+    }
+
+}
