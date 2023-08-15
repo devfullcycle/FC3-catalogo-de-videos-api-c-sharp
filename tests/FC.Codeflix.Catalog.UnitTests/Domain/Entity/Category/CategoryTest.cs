7d9bde281db92ca67c203ffabc4c64505cae06fc
Igor Gomes <igorgomes96@hotmail.com>
2023-08-15 16:47:24 -0300

    1.03 - Category entity unit tests

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,89 @@
+﻿using FC.Codeflix.Catalog.Domain.Exceptions;
+using FluentAssertions;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Category;
+
+[Collection(nameof(CategoryTestFixture))]
+public class CategoryTest
+{
+    private readonly CategoryTestFixture _categoryTestFixture;
+
+    public CategoryTest(CategoryTestFixture categoryTestFixture)
+        => _categoryTestFixture = categoryTestFixture;
+
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void Instantiate()
+    {
+        var validCategory = _categoryTestFixture.GetValidCategory();
+        var datetimeBefore = DateTime.Now;
+
+        var category = new DomainEntity.Category(validCategory.Name, validCategory.Description);
+        var datetimeAfter = DateTime.Now.AddSeconds(1);
+
+        category.Should().NotBeNull();
+        category.Name.Should().Be(validCategory.Name);
+        category.Description.Should().Be(validCategory.Description);
+        category.Id.Should().NotBeEmpty();
+        category.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+        (category.CreatedAt >= datetimeBefore).Should().BeTrue();
+        (category.CreatedAt <= datetimeAfter).Should().BeTrue();
+        (category.IsActive).Should().BeTrue();
+    }
+
+
+    [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithIsActive(bool isActive)
+    {
+        var validCategory = _categoryTestFixture.GetValidCategory();
+        var datetimeBefore = DateTime.Now;
+
+        var category = new DomainEntity.Category(validCategory.Name, validCategory.Description, isActive);
+        var datetimeAfter = DateTime.Now.AddSeconds(1);
+
+        category.Should().NotBeNull();
+        category.Name.Should().Be(validCategory.Name);
+        category.Description.Should().Be(validCategory.Description);
+        category.Id.Should().NotBeEmpty();
+        category.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+        (category.CreatedAt >= datetimeBefore).Should().BeTrue();
+        (category.CreatedAt <= datetimeAfter).Should().BeTrue();
+        (category.IsActive).Should().Be(isActive);
+    }
+
+    [Theory(DisplayName = nameof(InstantiateErrorWhenNameIsEmpty))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("   ")]
+    public void InstantiateErrorWhenNameIsEmpty(string? name)
+    {
+        var validCategory = _categoryTestFixture.GetValidCategory();
+
+        Action action =
+            () => new DomainEntity.Category(name!, validCategory.Description);
+
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("Name should not be empty or null");
+    }
+
+    [Fact(DisplayName = nameof(InstantiateErrorWhenDescriptionIsNull))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void InstantiateErrorWhenDescriptionIsNull()
+    {
+        var validCategory = _categoryTestFixture.GetValidCategory();
+
+        Action action =
+            () => new DomainEntity.Category(validCategory.Name, null!);
+
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("Description should not be null");
+    }
+}
+
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTestFixture.cs
@@ -0,0 +1,41 @@
+﻿using Bogus;
+using FC.Codeflix.Catalog.UnitTests.Common;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Category;
+public class CategoryTestFixture : BaseFixture
+{
+    public CategoryTestFixture()
+        : base() { }
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription =
+            Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10_000)
+            categoryDescription =
+                categoryDescription[..10_000];
+        return categoryDescription;
+    }
+
+    public DomainEntity.Category GetValidCategory()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription()
+        );
+}
+
+[CollectionDefinition(nameof(CategoryTestFixture))]
+public class CategoryTestFixtureCollection
+    : ICollectionFixture<CategoryTestFixture>
+{ }
