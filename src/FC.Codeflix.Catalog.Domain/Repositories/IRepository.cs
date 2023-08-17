f3f82f3ff59b1c9c8111058bad29c355e8a21fda
Igor Gomes <igorgomes96@hotmail.com>
2023-08-17 14:19:29 -0300

    1.11 - SearchCategory use case test fixture

--- src/FC.Codeflix.Catalog.Domain/Repositories/IRepository.cs
+++ src/FC.Codeflix.Catalog.Domain/Repositories/IRepository.cs
@@ -4,7 +4,6 @@ namespace FC.Codeflix.Catalog.Domain.Repositories;
 public interface IRepository<T> where T : class
 {
     Task SaveAsync(T entity, CancellationToken cancellationToken);
-    Task<T> GetAsync(Guid id, CancellationToken cancellationToken);
     Task DeleteAsync(Guid id, CancellationToken cancellationToken);
     Task<SearchOutput<T>> SearchAsync(
         SearchInput input,
