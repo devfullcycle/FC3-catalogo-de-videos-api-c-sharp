1d40ad43fb55e283a91c6b4f41e9c0787dc2c258
Igor Gomes <igorgomes96@hotmail.com>
2023-09-04 10:06:08 -0300

    3.02 - Creating service registrars

---
+++ src/FC.Codeflix.Catalog.Application/ServiceRegistrationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FC.Codeflix.Catalog.Application;
+internal static class ServiceRegistrationExtensions
+{
+    public static IServiceCollection AddUseCases(
+        this IServiceCollection services)
+    {
+        services.AddMediatR(x => x.RegisterServicesFromAssemblies(
+            typeof(ServiceRegistrationExtensions).Assembly));
+        return services;
+    }
+}
---
+++ src/FC.Codeflix.Catalog.Infra.Data.ES/ServiceRegistrationExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+
+namespace FC.Codeflix.Catalog.Infra.Data.ES;
+public static class ServiceRegistrationExtensions
+{
+    public static IServiceCollection AddElasticSearch(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration
+            .GetConnectionString("ElasticSearch");
+        var uri = new Uri(connectionString!);
+        var connectionSettings = new ConnectionSettings(uri)
+            //.DefaultMappingFor<CategoryModel>(i => i
+            //    .IndexName(ElasticsearchIndexes.Category)
+            //    .IdProperty(p => p.Id)
+            //)
+            //.EnableDebugMode()
+            .PrettyJson()
+            .ThrowExceptions()
+            .RequestTimeout(TimeSpan.FromMinutes(2));
+
+        var client = new ElasticClient(connectionSettings);
+        services.AddSingleton<IElasticClient>(client);
+        return services;
+    }
+
+}
