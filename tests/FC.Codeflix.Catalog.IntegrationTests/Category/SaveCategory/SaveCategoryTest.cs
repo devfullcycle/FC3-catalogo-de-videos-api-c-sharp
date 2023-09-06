326760ca5fbf3d04baf0c04a2bb418a843a7a98b
Igor Gomes <igorgomes96@hotmail.com>
2023-09-06 10:07:25 -0300

    3.04 - SaveCategory test - success scenario

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Category/SaveCategory/SaveCategoryTest.cs
@@ -0,0 +1,46 @@
+﻿using FC.Codeflix.Catalog.Domain.Exceptions;
+using FluentAssertions;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Category.SaveCategory;
+[Collection(nameof(SaveCategoryTestFixture))]
+public class SaveCategoryTest
+{
+    private readonly SaveCategoryTestFixture _fixture;
+
+    public SaveCategoryTest(SaveCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(SaveCategory_WhenInputIsValid_PersitsCategory))]
+    [Trait("Integration", "[UseCase] SaveCategory")]
+    public async Task SaveCategory_WhenInputIsValid_PersitsCategory()
+    {
+        var serviceProvider = _fixture.ServiceProvider;
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+        var elasticClient = serviceProvider.GetRequiredService<IElasticClient>();
+        var input = _fixture.GetValidInput();
+
+        var output = await mediator.Send(input);
+
+        var persisted = await elasticClient
+            .GetAsync<CategoryModel>(input.Id);
+        persisted.Found.Should().BeTrue();
+        var document = persisted.Source;
+        document.Should().NotBeNull();
+        document.Id.Should().Be(input.Id);
+        document.Name.Should().Be(input.Name);
+        document.Description.Should().Be(input.Description);
+        document.IsActive.Should().Be(input.IsActive);
+        document.CreatedAt.Should().Be(input.CreatedAt);
+        output.Should().NotBeNull();
+        output.Id.Should().Be(input.Id);
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
+        output.CreatedAt.Should().Be(input.CreatedAt);
+    }
+}
