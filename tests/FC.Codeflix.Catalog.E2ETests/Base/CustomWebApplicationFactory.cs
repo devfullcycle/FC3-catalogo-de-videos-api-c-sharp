512e823e494294277078ac93981d7c7c21dd23c5
Igor Gomes <igorgomes96@hotmail.com>
2023-09-26 17:55:19 -0300

    4.07 - Adding e2e test project to solution

---
+++ tests/FC.Codeflix.Catalog.E2ETests/Base/CustomWebApplicationFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace FC.Codeflix.Catalog.E2ETests.Base;
+public class CustomWebApplicationFactory<TStartup>
+    : WebApplicationFactory<TStartup>
+    where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        var environment = "EndToEndTest";
+        Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", environment);
+        builder.UseEnvironment(environment);
+        builder.ConfigureServices(services =>
+        {
+
+        });
+        base.ConfigureWebHost(builder);
+    }
+}
