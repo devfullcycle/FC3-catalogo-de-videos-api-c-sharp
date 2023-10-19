39aeab3479d43f21586f9076d2f00eef4c2c7ba5
Igor Gomes <igorgomes96@hotmail.com>
2023-10-19 10:25:34 -0300

    6.06 - Update Operation - e2e test

--- tests/FC.Codeflix.Catalog.E2ETests/Consumers/Category/CategoryConsumerTest.cs
+++ tests/FC.Codeflix.Catalog.E2ETests/Consumers/Category/CategoryConsumerTest.cs
@@ -1,5 +1,6 @@
 using FC.Codeflix.Catalog.Infra.Data.ES.Models;
 using FluentAssertions;
+using Nest;
 
 namespace FC.Codeflix.Catalog.E2ETests.Consumers.Category;
 
@@ -19,6 +20,32 @@ public class CategoryConsumerTest : IDisposable
     {
         var message = _fixture.BuildValidMessage("c");
         var category = message.Payload.After;
+        
+        await _fixture.PublishMessageAsync(message);
+        await Task.Delay(2_000);
+        
+        var persisted = await _fixture.ElasticClient
+            .GetAsync<CategoryModel>(category.Id);
+        persisted.Found.Should().BeTrue();
+        var document = persisted.Source;
+        document.Should().NotBeNull();
+        document.Id.Should().Be(category.Id);
+        document.Name.Should().Be(category.Name);
+        document.Description.Should().Be(category.Description);
+        document.IsActive.Should().Be(category.IsActive);
+        document.CreatedAt.Date.Should().Be(category.CreatedAt.Date);
+    }
+    
+    [Fact(DisplayName = nameof(CategoryEvent_WhenOperationIsUpdate_SavesCategory))]
+    [Trait("E2E/Consumers", "Category")]
+    public async Task CategoryEvent_WhenOperationIsUpdate_SavesCategory()
+    {
+        var examplesList = _fixture.GetCategoryModelList();
+        await _fixture.ElasticClient.IndexManyAsync(examplesList);
+        var example = examplesList[2];
+        var message = _fixture.BuildValidMessage("u", example);
+        var category = message.Payload.After;
+        category.Name = _fixture.DataGenerator.GetValidCategoryName();
         
         await _fixture.PublishMessageAsync(message);
         await Task.Delay(2_000);
