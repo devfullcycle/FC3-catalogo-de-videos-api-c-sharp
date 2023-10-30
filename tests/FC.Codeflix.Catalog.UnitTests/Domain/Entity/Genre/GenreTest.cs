02bd1d898e143a1bdba5510458f9de6c8a7eb5af
Igor Gomes <igorgomes96@hotmail.com>
2023-10-30 12:03:13 -0300

    7.03 - Genre unit tests

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Genre/GenreTest.cs
@@ -0,0 +1,92 @@
+using FC.Codeflix.Catalog.Domain.Exceptions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Genre;
+[Collection(nameof(GenreTestFixture))]
+public class GenreTest
+{
+    private readonly GenreTestFixture _fixture;
+
+    public GenreTest(GenreTestFixture fixture)
+        => _fixture = fixture;
+    
+    [Fact(DisplayName = nameof(Constructor_WhenValidArguments_Instantiate))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void Constructor_WhenValidArguments_Instantiate()
+    {
+        var example = _fixture.GetValidGenre();
+
+        var genre = new Catalog.Domain.Entity.Genre(
+            example.Id,
+            example.Name,
+            example.IsActive,
+            example.CreatedAt,
+            example.Categories);
+
+        genre.Id.Should().Be(example.Id);
+        genre.Name.Should().Be(example.Name);
+        genre.IsActive.Should().Be(example.IsActive);
+        genre.CreatedAt.Should().Be(example.CreatedAt);
+        genre.Categories.Should().BeEquivalentTo(example.Categories);
+        genre.CreatedAt.Should().Be(example.CreatedAt);
+    }
+    
+    [Fact(DisplayName = nameof(Constructor_WhenCategoriesIsNull_Instantiate))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void Constructor_WhenCategoriesIsNull_Instantiate()
+    {
+        var example = _fixture.GetValidGenre();
+
+        var genre = new Catalog.Domain.Entity.Genre(
+            example.Id,
+            example.Name,
+            example.IsActive,
+            example.CreatedAt,
+            null);
+
+        genre.Id.Should().Be(example.Id);
+        genre.Name.Should().Be(example.Name);
+        genre.IsActive.Should().Be(example.IsActive);
+        genre.CreatedAt.Should().Be(example.CreatedAt);
+        genre.Categories.Should().BeEmpty();
+        genre.CreatedAt.Should().Be(example.CreatedAt);
+    }
+    
+    [Fact(DisplayName = nameof(Constructor_WhenInvalidId_ThrowsException))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void Constructor_WhenInvalidId_ThrowsException()
+    {
+        var example = _fixture.GetValidGenre();
+
+        var action = () 
+            => new Catalog.Domain.Entity.Genre(
+                Guid.Empty,
+                example.Name,
+                example.IsActive,
+                example.CreatedAt,
+                example.Categories);
+
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("Id should not be empty or null");
+    }
+    
+    [Fact(DisplayName = nameof(Constructor_WhenInvalidId_ThrowsException))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void Constructor_WhenInvalidName_ThrowsException()
+    {
+        var example = _fixture.GetValidGenre();
+
+        var action = () 
+            => new Catalog.Domain.Entity.Genre(
+                example.Id,
+                null,
+                example.IsActive,
+                example.CreatedAt,
+                example.Categories);
+
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("Name should not be empty or null");
+    }
+
+}
