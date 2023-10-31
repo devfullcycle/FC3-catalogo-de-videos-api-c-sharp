381a2bd9daff31c5bbf56ae43e5f9c19c7f35bf9
Igor Gomes <igorgomes96@hotmail.com>
2023-10-31 10:36:11 -0300

    7.06 - Genre use cases test fixtures

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UseCases/Genre/SaveGenre/SaveGenreUseCaseTestFixture.cs
@@ -0,0 +1,33 @@
+using FC.Codeflix.Catalog.UnitTests.Application.UseCases.Genre.Common;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UseCases.Genre.SaveGenre;
+
+public class SaveGenreUseCaseTestFixture : GenreUseCaseTestFixture
+{
+    public SaveGenreInput GetValidInput()
+    {
+        var genre = DataGenerator.GetValidGenre();
+        return new(
+            genre.Id,
+            genre.Name,
+            genre.IsActive,
+            genre.CreatedAt,
+            genre.Categories.Select(item => new SaveGenreInputCategory(item.Id, item.Name)));
+    }
+
+    public SaveGenreInput GetInvalidInput()
+    {
+        var genre = DataGenerator.GetValidGenre();
+        return new(
+            genre.Id,
+            null!,
+            genre.IsActive,
+            genre.CreatedAt,
+            genre.Categories.Select(item => new SaveGenreInputCategory(item.Id, item.Name)));
+    }
+}
+
+[CollectionDefinition(nameof(SaveGenreUseCaseTestFixture))]
+public class SaveGenreUseCaseTestFixtureCollection
+    : ICollectionFixture<SaveGenreUseCaseTestFixture>
+{ }
