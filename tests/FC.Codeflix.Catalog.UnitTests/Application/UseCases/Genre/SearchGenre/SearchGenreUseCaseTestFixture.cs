46e61a9f1c37a74c9a201ce7262f1c7661ec9bc1
Igor Gomes <igorgomes96@hotmail.com>
2023-11-07 09:47:20 -0300

    7.11- Search Genre use case tests

--- tests/FC.Codeflix.Catalog.UnitTests/Application/UseCases/Genre/SearchGenre/SearchGenreUseCaseTestFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UseCases/Genre/SearchGenre/SearchGenreUseCaseTestFixture.cs
@@ -5,18 +5,18 @@ namespace FC.Codeflix.Catalog.UnitTests.Application.UseCases.Genre.SearchGenre;
 
 public class SearchGenreUseCaseTestFixture : GenreUseCaseTestFixture
 {
-    // public SearchGenreInput GetSearchInput()
-    // {
-    //     var random = new Random();
-    //     return new SearchGenreInput(
-    //         page: random.Next(1, 10),
-    //         perPage: random.Next(10, 20),
-    //         search: DataGenerator.Faker.Commerce.ProductName(),
-    //         orderBy: DataGenerator.Faker.Commerce.ProductName(),
-    //         order: random.Next(0, 2) == 0 
-    //             ? SearchOrder.Asc
-    //             : SearchOrder.Desc);
-    // }
+    public SearchGenreInput GetSearchInput()
+    {
+        var random = new Random();
+        return new SearchGenreInput(
+            page: random.Next(1, 10),
+            perPage: random.Next(10, 20),
+            search: DataGenerator.Faker.Commerce.ProductName(),
+            orderBy: DataGenerator.Faker.Commerce.ProductName(),
+            order: random.Next(0, 2) == 0 
+                ? SearchOrder.Asc
+                : SearchOrder.Desc);
+    }
 
     public List<DomainEntity.Genre> GetGenreList(int length = 10)
         => Enumerable
