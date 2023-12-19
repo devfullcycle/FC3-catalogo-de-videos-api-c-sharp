0660de120e84a5e4f891ccce51149d28af079286
Igor Gomes <igorgomes96@hotmail.com>
2023-12-19 11:25:09 -0300

    10.12 - Retries - Part 3

--- src/FC.Codeflix.Catalog.Infra.Messaging/Configuration/KafkaConfiguration.cs
+++ src/FC.Codeflix.Catalog.Infra.Messaging/Configuration/KafkaConfiguration.cs
@@ -14,6 +14,9 @@ public class KafkaConsumerConfiguration
     public string Topic { get; set; } = null!;
     public string? RetryTopic { get; set; }
     public string? DlqTopic { get; set; }
+    public int ConsumeDelaySeconds { get; private set; }
+
+    public bool HasRetry => !string.IsNullOrWhiteSpace(RetryTopic);
 
     public KafkaConsumerConfiguration CreateRetryConfiguration(int retryIndex, bool hasNextRetry)
         => new()
@@ -22,7 +25,8 @@ public class KafkaConsumerConfiguration
             GroupId = GroupId,
             Topic = Topic.ToRetryTopic(retryIndex),
             RetryTopic = !hasNextRetry ? null : Topic.ToRetryTopic(retryIndex + 1),
-            DlqTopic = DlqTopic
+            DlqTopic = DlqTopic,
+            ConsumeDelaySeconds = (int)Math.Pow(2, retryIndex)
         };
 
 }
--- src/FC.Codeflix.Catalog.Infra.Messaging/Consumers/KafkaConsumer.cs
+++ src/FC.Codeflix.Catalog.Infra.Messaging/Consumers/KafkaConsumer.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using Confluent.Kafka;
 using Confluent.Kafka.Admin;
+using FC.Codeflix.Catalog.Domain.Exceptions;
 using FC.Codeflix.Catalog.Infra.Messaging.Common;
 using FC.Codeflix.Catalog.Infra.Messaging.Configuration;
 using FC.Codeflix.Catalog.Infra.Messaging.Models;
@@ -16,7 +17,7 @@ public class KafkaConsumer<TMessage> : BackgroundService
     private readonly KafkaConsumerConfiguration _configuration;
     private readonly ILogger<KafkaConsumer<TMessage>> _logger;
     private readonly IServiceProvider _serviceProvider;
-
+    private readonly IProducer<string, string> _producer;
     private readonly List<(Func<MessageModel<TMessage>, bool> Predicate, Type Type)> _messageHandlers = new();
 
     public KafkaConsumer(
@@ -27,7 +28,15 @@ public class KafkaConsumer<TMessage> : BackgroundService
         _configuration = configuration;
         _logger = logger;
         _serviceProvider = serviceProvider;
+        _producer = new ProducerBuilder<string, string>(GetProducerConfig()).Build();
     }
+
+    private ProducerConfig GetProducerConfig()
+        => new()
+        {
+            BootstrapServers = _configuration.BootstrapServers,
+            Acks = Acks.All
+        };
 
     private ConsumerConfig GetConsumerConfig()
         => new()
@@ -54,6 +63,11 @@ public class KafkaConsumer<TMessage> : BackgroundService
             {
                 continue;
             }
+
+            if (_configuration.ConsumeDelaySeconds > 0)
+            {
+                await Task.Delay(_configuration.ConsumeDelaySeconds * 1_000, stoppingToken);
+            }
             await HandleMessageAsync(consumeResult.Message, stoppingToken);
             consumer.StoreOffset(consumeResult);
         }
@@ -62,31 +76,56 @@ public class KafkaConsumer<TMessage> : BackgroundService
 
     private async Task HandleMessageAsync(Message<string, string> message, CancellationToken cancellationToken)
     {
-        using var scope = _serviceProvider.CreateScope();
-
-        var messageModel = JsonSerializer.Deserialize<MessageModel<TMessage>>(
-            message.Value, SerializerConfiguration.JsonSerializerOptions);
-
-        var messageHandlerType = _messageHandlers
-            .FirstOrDefault(tuple => tuple.Predicate.Invoke(messageModel!))
-            .Type;
-
-        if (messageHandlerType is null)
+        try
         {
-            _logger.LogError("None of the message handlers predicates was satisfied by the message: {@message}",
-                message);
+            using var scope = _serviceProvider.CreateScope();
+
+            var messageModel = JsonSerializer.Deserialize<MessageModel<TMessage>>(
+                message.Value, SerializerConfiguration.JsonSerializerOptions);
+
+            var messageHandlerType = _messageHandlers
+                .FirstOrDefault(tuple => tuple.Predicate.Invoke(messageModel!))
+                .Type;
+
+            if (messageHandlerType is null)
+            {
+                _logger.LogError("None of the message handlers predicates was satisfied by the message: {@message}",
+                    message);
+                return;
+            }
+
+            var handler = (IMessageHandler<TMessage>?)scope.ServiceProvider.GetRequiredService(messageHandlerType);
+
+            if (handler is null)
+            {
+                _logger.LogError("No message handle found of type: {@messageHandlerType}", messageHandlerType);
+                return;
+            }
+
+            await handler.HandleMessageAsync(messageModel!, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            await OnErrorAsync(ex, message, cancellationToken);
+        }
+    }
+
+    private async Task OnErrorAsync(Exception exception, Message<string, string> message,
+        CancellationToken cancellationToken)
+    {
+        if (exception is JsonException or BusinessRuleException)
+        {
+            _logger.LogError(exception, "Non-retryable error. Sending to DLQ. {error}", exception.Message);
+            await _producer.ProduceAsync(_configuration.DlqTopic, message, cancellationToken);
             return;
         }
 
-        var handler = (IMessageHandler<TMessage>?)scope.ServiceProvider.GetRequiredService(messageHandlerType);
-
-        if (handler is null)
-        {
-            _logger.LogError("No message handle found of type: {@messageHandlerType}", messageHandlerType);
-            return;
-        }
+        string topic = _configuration.HasRetry
+            ? _configuration.RetryTopic!
+            : _configuration.DlqTopic!;
         
-        await handler.HandleMessageAsync(messageModel!, cancellationToken);
+        _logger.LogError(exception, "Retryable error. Sending to {topic}. Error: {error}", topic, exception.Message);
+        await _producer.ProduceAsync(topic, message, cancellationToken);
     }
 
     public void AddMessageHandler<T>(Func<MessageModel<TMessage>, bool> predicate)
@@ -119,5 +158,11 @@ public class KafkaConsumer<TMessage> : BackgroundService
         {
             _logger.LogWarning("Topic already exists: {error}", ex.ToString());
         }
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _producer.Dispose();
+        return base.StopAsync(cancellationToken);
     }
 }
