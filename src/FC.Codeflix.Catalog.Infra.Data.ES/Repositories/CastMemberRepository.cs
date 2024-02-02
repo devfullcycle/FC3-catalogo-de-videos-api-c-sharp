e3b351a2cc9b682c4dfbfab8f23cd24ebc957af5
Igor Gomes <igorgomes96@hotmail.com>
2024-02-02 10:18:52 -0300

    13.07, 13.08 - SeachCastMember repository and integration tests

--- src/FC.Codeflix.Catalog.Infra.Data.ES/Repositories/CastMemberRepository.cs
+++ src/FC.Codeflix.Catalog.Infra.Data.ES/Repositories/CastMemberRepository.cs
@@ -32,8 +32,52 @@ public class CastMemberRepository : ICastMemberRepository
         }
     }
 
-    public Task<SearchOutput<CastMember>> SearchAsync(SearchInput input, CancellationToken cancellationToken)
+    public async Task<SearchOutput<CastMember>> SearchAsync(SearchInput input, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var response = await _client
+            .SearchAsync<CastMemberModel>(s => s
+                    .Query(q => q
+                        .Match(m => m
+                            .Field(f => f.Name)
+                            .Query(input.Search)
+                        )
+                    )
+                    .From(input.From)
+                    .Size(input.PerPage)
+                    .Sort(BuildSortExpression(input.OrderBy, input.Order)),
+                ct: cancellationToken);
+
+        var castMembers = response.Documents
+            .Select(doc => doc.ToEntity())
+            .ToList();
+
+        return new SearchOutput<CastMember>(
+            input.Page,
+            input.PerPage,
+            (int)response.Total,
+            castMembers);
     }
+    
+    private static Func<SortDescriptor<CastMemberModel>, IPromise<IList<ISort>>> BuildSortExpression(
+        string orderBy, SearchOrder order)
+        => (orderBy.ToLower(), order) switch
+        {
+            ("name", SearchOrder.Asc) => sort => sort
+                .Ascending(f => f.Name.Suffix("keyword"))
+                .Ascending(f => f.Id),
+            ("name", SearchOrder.Desc) => sort => sort
+                .Descending(f => f.Name.Suffix("keyword"))
+                .Descending(f => f.Id),
+            ("id", SearchOrder.Asc) => sort => sort
+                .Ascending(f => f.Id),
+            ("id", SearchOrder.Desc) => sort => sort
+                .Descending(f => f.Id),
+            ("createdat", SearchOrder.Asc) => sort => sort
+                .Ascending(f => f.CreatedAt),
+            ("createdat", SearchOrder.Desc) => sort => sort
+                .Descending(f => f.CreatedAt),
+            _ => sort => sort
+                .Ascending(f => f.Name.Suffix("keyword"))
+                .Ascending(f => f.Id)
+        };
 }
