8752de8dd1b6952db87c501adb61710a00600018
Igor Gomes <igorgomes96@hotmail.com>
2024-02-06 10:23:44 -0300

    14.02 - Update schema.graphql for e2e tests

---
+++ sandbox/start.sh
@@ -0,0 +1,5 @@
+docker network create --driver bridge catalog_api || true
+docker network create --driver bridge service_catalog || true
+
+docker-compose -f ./elk/docker-compose.yml up -d 
+docker-compose -f ./kafka/docker-compose.yml up -d 
