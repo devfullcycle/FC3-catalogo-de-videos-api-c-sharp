a8be3d1a6393c396288553b1d967c6e46502ae3d
Igor Gomes <igor.gomes@windifferent.com>
2024-03-08 10:34:43 -0300

    17.03 - Save Video integration test

--- tests/FC.Codeflix.Catalog.IntegrationTests/Fakes/AdminCatalogGatewayFake.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Fakes/AdminCatalogGatewayFake.cs
@@ -8,14 +8,20 @@ public class AdminCatalogGatewayFake
     : IAdminCatalogGateway
 {
     private readonly GenreDataGenerator _genreDataGenerator = new();
+    private readonly CategoryDataGenerator _categoryDataGenerator = new();
+    private readonly CastMemberDataGenerator _castMemberDataGenerator = new();
     private readonly VideoDataGenerator _videoDataGenerator = new();
     public Task<Domain.Entity.Genre> GetGenreAsync(Guid id, CancellationToken cancellationToken)
     {
         return Task.FromResult(_genreDataGenerator.GetValidGenre(id));
     }
 
-    public Task<Video> GetVideoAsync(Guid id, CancellationToken cancellationToken)
+    public Task<Domain.Entity.Video> GetVideoAsync(Guid id, CancellationToken cancellationToken)
     {
-        return Task.FromResult(_videoDataGenerator.GetValidVideo());
+        var video = _videoDataGenerator.GetValidVideo(id);
+        video.AddCategories(_categoryDataGenerator.GetValidCategory());
+        video.AddGenres(_genreDataGenerator.GetValidGenre());
+        video.AddCastMembers(_castMemberDataGenerator.GetValidCastMember());
+        return Task.FromResult(video);
     }
 }
